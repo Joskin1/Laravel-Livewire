3d19bc0ce15dab1cfbabab7e5e32f500662fb658
joskin <joskinjoseph1@gmail.com>
2024-03-26 19:41:02 +0100

    loasding state

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -4,32 +4,50 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use Livewire\WithPagination;
 
 class Clicker extends Component
 {
+    use WithFileUploads;
     use WithPagination;
+
     public $name;
     public $email;
     public $password;
-    public function createNewUser(){
-        $this->validate([
-            "name"=> "required|min:2|max:50",
-            "email"=> "required|unique:users",
-            "password"=> "required|min:3"
-        ]);
-       User::create([
-        'name' => $this->name,
-        'email' => $this->email,
-        'password'=> bcrypt($this->password),
-       ]);
-       request()->session()->flash('success','Registration is successful');
+    public $image;
+
+    protected $rules = [
+        "name" => "required|min:2|max:50",
+        "email" => "required|unique:users",
+        "password" => "required|min:3",
+        'image' => 'nullable|sometimes|image|max:1024', // Maximum size is 1 MB
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
     }
+
+    public function createNewUser()
+    {
+        $validated = $this->validate();
+
+        if ($this->image) {
+            $validated['image'] = $this->image->store('uploads', 'public');
+        }
+
+        User::create($validated);
+
+        $this->reset(['name', 'email', 'password', 'image']);
+
+        session()->flash('success', 'Registration is successful');
+    }
+
     public function render()
     {
         $users = User::paginate(2);
-        return view('livewire.clicker', [
-            'users'=> $users,
-        ]);
+
+        return view('livewire.clicker', compact('users'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <script src="https://cds.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 
-    
+
     </head>
     <body>
         @livewire('clicker')
