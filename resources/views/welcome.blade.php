d47ffeb2d609adc99f33bd784f4271851ce1910a
joskin <joskinjoseph1@gmail.com>
2024-03-29 01:26:00 +0100

    Component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <script src="https://cdn.tailwindcss.com"></script>
-
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
-
-<body class="antialiased">
-
-    <div class="flex">
-        <div class="w-full">
-            @livewire('users-list', ['search' => 'prof'])
-        </div>
-        {{-- <div class="w-2/4">
-            @livewire('register-form')
-        </div> --}}
-    </div>
-
+<body>
+ghiop'[]klq
 </body>
-
 </html>
